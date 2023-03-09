dd33ce35fa33525a6bae9bd7e367485f717eaea3
Arsham <arsham.ramezani@gmail.com>
2023-03-09 18:20:19 +0330

    feat: update website title

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,43 +1,37 @@
 /** @jsxImportSource @emotion/react */
 
-import { css } from '@emotion/react'
 import React from 'react'
 import leftImg from '../../assets/background/left-background-img.svg'
 import rightImg from '../../assets/background/right-background-img.svg'
+import styled from '@emotion/styled'
 interface LayoutProps {
   children?: React.ReactNode
 }
+const LayoutBody = styled('div')`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  background-color: #1cc976;
+`
+const LeftPartImage = styled('img')`
+  position: absolute;
+  top: 0;
+  left: 6px;
+`
+const RightPartImage = styled('img')`
+position:absolutel
+top:0;
+right:6px;`
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div
-      css={css`
-        position: relative;
-        width: 100%;
-        min-height: 100vh;
-        display: flex;
-        justify-content: center;
-        background-color: #1cc976;
-      `}>
-      <img
-        alt="left-part"
-        src={leftImg}
-        css={css({
-          position: 'absolute',
-          top: 0,
-          left: 6
-        })}
-      />
+    <LayoutBody>
+      <LeftPartImage alt="left-part" src={leftImg} />
       {children}
-      <img
-        alt="right-part"
-        src={rightImg}
-        css={css({
-          position: 'absolute',
-          top: 0,
-          right: 6
-        })}
-      />
-    </div>
+      <RightPartImage alt="right-part" src={rightImg} />
+    </LayoutBody>
   )
 }
 export default Layout
